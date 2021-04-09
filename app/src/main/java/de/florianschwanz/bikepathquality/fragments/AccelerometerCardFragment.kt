49c9e0e7545ea81d1c194249909c20800e17d1bf
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-04-09 12:43:01 +0200

    refactor: use constraint layout instead of linear layout

--- app/src/main/java/de/florianschwanz/bikepathquality/fragments/AccelerometerCardFragment.kt
+++ app/src/main/java/de/florianschwanz/bikepathquality/fragments/AccelerometerCardFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.common.collect.EvictingQueue
 import de.florianschwanz.bikepathquality.R
+import java.lang.Math.abs
 
 /**
  * Accelerometer card fragment
@@ -29,17 +31,38 @@ class AccelerometerCardFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.accelerometer_card_fragment, container, false)
 
+        val tvMinusX: TextView = view.findViewById(R.id.tvMinusX)
+        val tvMinusY: TextView = view.findViewById(R.id.tvMinusY)
+        val tvMinusZ: TextView = view.findViewById(R.id.tvMinusZ)
         val tvAccelerometerX: TextView = view.findViewById(R.id.tvAccelerometerX)
         val tvAccelerometerY: TextView = view.findViewById(R.id.tvAccelerometerY)
         val tvAccelerometerZ: TextView = view.findViewById(R.id.tvAccelerometerZ)
 
+        val accelerometerEvictingQueueX: EvictingQueue<Float> = EvictingQueue.create(100)
+        val accelerometerEvictingQueueY: EvictingQueue<Float> = EvictingQueue.create(100)
+        val accelerometerEvictingQueueZ: EvictingQueue<Float> = EvictingQueue.create(100)
+
         viewModel = ViewModelProvider(requireActivity()).get(AccelerometerCardViewModel::class.java)
         viewModel.data.observe(viewLifecycleOwner, {
-            tvAccelerometerX.text = resources.getString(R.string.accelerometer_x, it.x)
-            tvAccelerometerY.text = resources.getString(R.string.accelerometer_y, it.y)
-            tvAccelerometerZ.text = resources.getString(R.string.accelerometer_z, it.z)
+
+            accelerometerEvictingQueueX.add(it.x)
+            accelerometerEvictingQueueY.add(it.y)
+            accelerometerEvictingQueueZ.add(it.z)
+
+            val x = accelerometerEvictingQueueX.average()
+            val y = accelerometerEvictingQueueY.average()
+            val z = accelerometerEvictingQueueZ.average()
+
+            tvMinusX.text = x.sign()
+            tvMinusY.text = y.sign()
+            tvMinusZ.text = z.sign()
+            tvAccelerometerX.text = resources.getString(R.string.accelerometer_x, abs(x))
+            tvAccelerometerY.text = resources.getString(R.string.accelerometer_y, abs(y))
+            tvAccelerometerZ.text = resources.getString(R.string.accelerometer_z, abs(z))
         })
 
         return view
     }
+
+    private fun Double.sign(): String = if (this < 0) "-" else ""
 }
